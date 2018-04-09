aa83aa931911ce8b61a15b990e6643a160b9c939
Brad Sneade <brad@livesafemobile.com>
2018-04-09 16:56:17 -0400

    Making database access info a bit clearer

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-heroku login
-heroku create
-heroku addons:create heroku-postgresql:hobby-dev
-git push heroku master
+#heroku login
+#heroku create
+#heroku addons:create heroku-postgresql:hobby-dev
+#git push heroku master
 heroku run "npm run initdb"
+echo "Here is your database connection information.  It is in the format postgres://<user>:<password>@<host>:<port>/<database>"
+heroku config
 heroku open
